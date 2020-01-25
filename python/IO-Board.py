9fc71ddfde0bd94b1ca9f4d00c6246793cbd89d4
Christof <chrak@cmja.de>
2020-01-25 15:12:06 +0100

    Kanalname eingeführt

--- python/IO-Board.py
+++ python/IO-Board.py
@@ -3,10 +3,12 @@ import sys, os
 from cmulti import CMULTI
 
 class IOBoard(CMULTI):
-  def __init__(self,source,target,comPort="", baudRate=57600, backChannel="TLog", withCrc = True, timeout=3):
-    super(self.__class__,self).__init__(source,comPort , baudRate, backChannel, withCrc, timeout)
+  def __init__(self,source,target,comPort="", baudRate=115200, withCrc = True, timeout=3):
+    super(self.__class__,self).__init__(source,comPort , baudRate, withCrc, timeout)
     self.target = target
     self.source = source
+    self.iname = {"sensor0":0,"sensor1":1,"sensor2":2,"sensor3":3}
+    self.oname = {"aktor0":0,"aktor1":1,"aktor2":2,"aktor3":3}
     
     
   def setSecurityKey(self,key):
@@ -32,6 +34,8 @@ class IOBoard(CMULTI):
     return( int( self.errorHandler(self.input()) )  )
 
   def setOutputState(self,bit,state):
+    if type(bit) == str:
+      bit = self.oname[bit]
     self.sendStandard(str(state),self.target,"Q",str(bit),"S","T")
     self.errorHandler(self.input()) 
     return True
@@ -47,6 +51,8 @@ class IOBoard(CMULTI):
       self.setOutputState(i,0)
 
   def getInputState(self,bit):
+    if type(bit) == str:
+      bit = self.iname[bit]
     self.sendCommand(self.target,"I",str(bit),"g")
     result = self.errorHandler( self.input() )
     if result == '0':
@@ -62,7 +68,13 @@ class IOBoard(CMULTI):
         result |= bit
       bit = bit << 1
     return(result)
-    
+   
+  def mapInputs(self,names):
+    self.iname = names 
+
+  def mapOutputs(self,names):
+    self.oname = names 
+
   def errorHandler(self,result):
     if result[3]==False:
       raise ValueError("Timeout, Gerät hat nicht in der vordefinierten Zeit geantwortet!")
@@ -71,21 +83,29 @@ class IOBoard(CMULTI):
     if result[1]==False:
       raise ValueError("Gerät gab Fehler zurück!")
     return(result[0])
-    
-test = IOBoard('PC','I1',comPort="/dev/ttyUSB0", baudRate=115200)
 
-test.open()
-print( test.getSecurityKey() )
-print( test.setSecurityKey("Phe6%!kdf?+2aQ") )
-print( test.getSecurityKey() )
-print( test.setSecurityKey("D=&27ane%24dez") )
-print( test.getCompilationDate() )
-print( test.getCompilationTime() )
-print( test.getFreeMemory() )
-print( test.setOutputState(2,1) )
-print( test.getInputState(0) )
-print( test.getAllInputs() )
-test.close()
+ichannel = {"LS_Eingang":0,"LS_Ausgang":1,"sensor3":2,"sensor4":3}
+ochannel = {"Zylinder":0,"aktor1":1,"aktor2":2,"aktor3":3}
+
+anlage = IOBoard('PC','I1',comPort="/dev/ttyUSB0")
+
+anlage.mapInputs(ichannel)
+anlage.mapOutputs(ochannel)
+
+anlage.open()
+print( anlage.getSecurityKey() )
+print( anlage.setSecurityKey("Phe6%!kdf?+2aQ") )
+print( anlage.getSecurityKey() )
+print( anlage.setSecurityKey("D=&27ane%24dez") )
+print( anlage.getCompilationDate() )
+print( anlage.getCompilationTime() )
+print( anlage.getFreeMemory() )
+print( anlage.clearAllOutputs() )
+print( anlage.setOutputState( "Zylinder",1 ) )
+print( anlage.getInputState("LS_Eingang") )
+print( anlage.getInputState("LS_Ausgang") )
+print( anlage.getAllInputs() )
+anlage.close()
 
 #print( test.clearAllOutputs() )
 
